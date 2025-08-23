010472e7548df5c040efc9eb815135fdd706def8
bhaktichopra2 <bhakti.aps@gmail.com>
2025-08-23 19:16:18 +0530

    Challenge 2 Day 30 done

---
+++ Day 30/Challenge 2/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        char current = 'a';
+        int totalrot = 0;
+
+        for(char ch : s.toCharArray()){
+            int diff = Math.abs(current - ch);
+            int rot = Math.min(diff, 26-diff);
+            totalrot += rot;
+            current = ch;
+        }
+        System.out.println(totalrot);
+        sc.close();
+    }
+}
